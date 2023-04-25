5e02a21bf5142312abd0d150b28a665e99048d02
Youssef <yuossefbakier@gmail.com>
2023-04-25 13:07:14 -0700

    Finish Task

--- precision_Y.c
+++ precision_Y.c
@@ -1,40 +1,41 @@
 #include "main.h"
 
 /**
- * get_precision - Calculate
- * @format: Formatted string
- * @i: List of arguments
- * @list: list
- * Return: Precision
+ * get_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	int cur_i = *i + 1;
-	int prec = -1;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-	if (format[cur_i] != '.')
-		return (prec);
+	if (format[curr_i] != '.')
+		return (precision);
 
-	prec = 0;
+	precision = 0;
 
-	for (cur_i += 1; format[cur_i] != '\0'; cur_i++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[cur_i]))
+		if (is_digit(format[curr_i]))
 		{
-			prec *= 10;
-			prec += format[cur_i] - '0';
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
-		else if (format[cur_i] == '*')
+		else if (format[curr_i] == '*')
 		{
-			cur_i++;
-			prec = va_arg(list, int);
+			curr_i++;
+			precision = va_arg(list, int);
 			break;
 		}
 		else
 			break;
 	}
 
-	*i = cur_i - 1;
+	*i = curr_i - 1;
 
-	return (prec);
+	return (precision);
 }
